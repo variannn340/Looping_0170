fbfc56256ccecd4ebc9d77b47d2adae5839512f8
variannn340 <varianvarian0301@gmail.com>
2026-04-07 12:35:08 +0700

    full logic and

--- logic_and.cpp
+++ logic_and.cpp
@@ -5,4 +5,17 @@ int main(){
     float nilB,nilM,rerata;
     string status;
 
-    
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) && (nilM >= 70 ))
+         status = "Lulus";
+    else
+         status = "Tidak Lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+}
