d6ce205178940df85c0b8c5058ef03930d1b8237
variannn340 <varianvarian0301@gmail.com>
2026-04-07 12:38:45 +0700

    tambahkan fungsi pilihJurusan untuk switch case

--- switchCase.cpp
+++ switchCase.cpp
@@ -15,6 +15,30 @@ void input() {
     cin >> kodeJurusan;
 }
 
+void pilihJurusan() {
+    switch (kodeJurusan) {
+
+        case 1:
+        namaJurusan = "Teknologi Informasi";
+        statusKuota = "Tersedia (15 Kursi)";
+        break;
+
+        case 2:
+        namaJurusan = "Teknik Elektro";
+        statusKuota = "Kuota Penuh!";
+        break;
+
+        case 3:
+        namaJurusan = "Sistem Informasi";
+        statusKuota = "Tersedia (5 Kursi)";
+        break;
+
+        default:
+        namaJurusan = "Tidak Diketahui";
+        statusKuota = "Error: Kode Tidak Valid";
+        break;
+    }
+}
 
 
 
