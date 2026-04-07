36f60b2fc80669b98d30b29e547ce70c075c1773
variannn340 <varianvarian0301@gmail.com>
2026-04-07 12:13:46 +0700

    tambah void input

--- switchCase.cpp
+++ switchCase.cpp
@@ -5,6 +5,20 @@ int kodeJurusan;
 string namaJurusan;
 string statusKuota;
 
+void input() {
+    cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
+    cout << "1. Teknologi Informasi" << endl;
+    cout << "2. Teknik Elektro" << endl;
+    cout << "3. Sistem Informasi" << endl;
+    cout << "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _" << endl;
+    cout << "Masukkan kode jurusan (1-3): ";
+    cin >> kodeJurusan;
+}
+
+
+
+
+
 
 
 
