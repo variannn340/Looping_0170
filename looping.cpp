cc2954fc393e9cf3db46a0c665f10080a93b9fa2
variannn340 <varianvarian0301@gmail.com>
2026-04-07 12:33:13 +0700

    set library untuk logic and

--- looping.cpp
+++ looping.cpp
@@ -35,33 +35,6 @@ int main(){
     cout<<endl;
 }
 
-
-
-
-
-
-#include <iostream>
-using namespace std;
-
-int main(){
-    float nilB,nilM,rerata;
-    string status;
-
-    cout<<"Masukkan nilai Matematika = ";
-    cin>>nilM;
-    cout<<"Masukkan nilai Bahasa Inggris = ";
-    cin>>nilB;
-
-    rerata = (nilB+nilM)/2;
-
-    if ((rerata >= 60) || (nilM >= 70 ))
-         status = "Lulus";
-    else
-         status = "Tidak Lulus";
-
-    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
-}
-
 #include <iostream>
 using namespace std;
 
